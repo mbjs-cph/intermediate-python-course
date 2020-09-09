10968c2fabb102ecf4390b1f6da42ab61906536d
Bilal Jamil <smb@Bilals-MacBook-Pro.local>
2020-09-09 12:48:47 +0200

    This is a test

--- dice_roller.py
+++ dice_roller.py
@@ -6,7 +6,13 @@ def main():
   for i in range(0,dice_rolls):
     roll = random.randint(1,6)
     dice_sum = dice_sum + roll # eller 'dice_sum += roll'
-    print(f'You rolled a {roll}')
+    #print(f'You rolled a {roll}')
+    if roll == 1:
+      print(f'You rolled a {roll}! Critical Fail')
+    elif roll == 6:
+      print(f'You rolled a {roll}! Critical Success!')
+    else:
+      print(f'You rolled a {roll}')
   print(f'You have rolled a total of {dice_sum}')
     #roll = 5
     #print('You rolled a die')
